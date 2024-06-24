083891e5012c57e3794985dab23b52e52403abd4
tony held <tony.held@arb.ca.gov>
2024-06-23 19:28:03 -0700

    adding flask/jinja macros

--- todo_list.py
+++ todo_list.py
@@ -1,6 +1,4 @@
 # my next steps
-# todo: read https://ttl255.com/jinja2-tutorial-part-1-introduction-and-variable-substitution/
-# todo: read https://documentation.bloomreach.com/engagement/docs/jinja-syntax
-# todo: read https://jinja.palletsprojects.com/en/latest/templates/
+
 
 
